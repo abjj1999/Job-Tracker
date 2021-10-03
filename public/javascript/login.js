17ee17d334e184d472c433be21603013ebbbf120
J <torres.j.isaias@gmail.com>
2021-10-03 15:16:40 -0500

    modularized js login signup

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -22,34 +22,4 @@ async function loginFormHandler(event) {
   }
 }
 
-async function signupFormHandler(event) {
-  event.preventDefault();
-
-  const firstName = document.querySelector('#first-signup').value.trim();
-  const lastName = document.querySelector('#last-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
-
-  if (firstName && lastName && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'post',
-      body: JSON.stringify({
-        firstName,
-        lastName,
-        email,
-        password
-      }),
-      headers: { 'Content-Type': 'application/json' }
-    });
-
-    if (response.ok) {
-      document.location.replace('/dashboard/');
-    } else {
-      alert(response.statusText);
-    }
-  }
-}
-
 document.querySelector('.login-form').addEventListener('submit', console.log("ok"));
-
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
