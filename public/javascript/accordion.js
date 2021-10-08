39fc7c2ae36c907a9df619c60f00362e442ef012
Zach Yarbrough <zachyarbro@gmail.com>
2021-10-08 15:45:09 -0500

    edit accordion so only one can be displayed at a time

--- public/javascript/accordion.js
+++ public/javascript/accordion.js
@@ -3,9 +3,18 @@ document.querySelectorAll('.accordion__button').forEach(button => {
         const accordionContent = button.nextElementSibling;
         
         button.classList.toggle('accordion__button--active');
-
         if(button.classList.contains('accordion__button--active')) {
-            accordionContent.setAttribute('style', 'overflow: auto; max-height: 350px;')
+            accordionContent.setAttribute('style', 'overflow: auto; max-height: 350px;');
+            
+            document.querySelectorAll('.accordion__button').forEach(btn => {
+                if(button !== btn) {
+                    const accContent = btn.nextElementSibling;
+                    if(btn.classList.contains('accordion__button--active')) {
+                        btn.classList.toggle('accordion__button--active');
+                        accContent.setAttribute('style', 'overflow: hidden; max-height: 0;');
+                    }
+                }
+            })
         } else {
             accordionContent.setAttribute('style', 'overflow: hidden; max-height: 0;');
         }
