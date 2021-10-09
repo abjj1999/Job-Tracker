a6043cd587f7f78a5df012514d57e6c55e8e26a0
Zach Yarbrough <zachyarbro@gmail.com>
2021-10-09 12:18:20 -0500

    add search bar funcitonality

--- public/javascript/filter.js
+++ public/javascript/filter.js
@@ -3,7 +3,13 @@ const filterBtn = $('.filterSearchBtn');
 const filterContainer = document.querySelector('.filter-container');
 
 filter.on('click', () => {
-    console.log('Clicked!')
+    const search = document.querySelector('#search');
+    if(window.location.href.includes('?search')) {
+        const searchArr = window.location.search.split('/');
+        const searchText = searchArr[searchArr.length - 1].split('=')[1];
+        search.value = searchText;
+    }
+
     if (filterContainer.classList.contains('filter-container')) {
         filterContainer.removeAttribute('class', 'filter-container')
         filterContainer.setAttribute('class', 'filter-container--active');
@@ -13,6 +19,12 @@ filter.on('click', () => {
 })
 
 filterBtn.on('click', () => {
-    console.log('yay')
+    const search = document.querySelector('#search').value.trim();
+    if(search) {
+        console.log(search)
+        document.location.replace(`/?search=${search}`);
+    } else {
+        document.location.replace(`/`);
+    }
 })
 
